647b57f64dd4843d1c85dedb53b0dc4bf968f03b
Yunhwan <79241793+yunhwan98@users.noreply.github.com>
2023-03-12 22:37:48 +0900

    [level 3] Title: 입국심사, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -2,33 +2,25 @@ def solution(n, times):
     answer = 0
     m = len(times)
     
+    dp = [[times[i]] for i in range(m)]
+    print(dp)
     #더 빨리 끝나는 심사대가 있으면 그 곳으로 갈 수 있음
     #모든 사람이 심사를 받는데 걸리는 시간을 최소
     
     
     #끝날 때 시간이 최소인 곳으로 배치를 하면됌
     
-
+    
+    # 7  : 7 14 21 28
+    # 10 : 10 20 
+    
+    
+    #2 :2 4 6 8 10 12 14
+    #4 :4 8 12
+    #5 :5 10
+    
+    
     sorted(times)
+    print(times)
     
-    start = 0
-    end = times[-1] * n
-    
-    #이분 탐색
-    while start<=end:
-        mid = (start+end)//2
-        sum_arr = 0
-        
-        #mid 시간동안 가능한 명수 구하기
-        for i in range(m):
-            sum_arr += mid//times[i]
-        
-        if sum_arr<n:
-            start=mid+1
-
-        else:
-            end = mid-1       
-            answer=mid
-            
-
     return answer
